cb21aadeacac2e4a267e7be6d652d03be3389c19
bxdong@ucdavis.edu <dongbinxin@toutakeshikins-MacBook-Pro.local>
2020-01-07 21:41:44 -0800

    debug like count

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Insta.models import Post, PostTwo, InstaUser, Like
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(PostTwo)
+admin.site.register(InstaUser)
+admin.site.register(Like)
+
