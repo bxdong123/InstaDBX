e25e47fe62db47673c68aa8f0cb3aee102499adf
bxdong@ucdavis.edu <dongbinxin@toutakeshikins-MacBook-Pro.local>
2020-01-12 21:27:13 -0800

    finish website development

--- Insta/views.py
+++ Insta/views.py
@@ -1,10 +1,10 @@
 #from django.shortcuts import render
 from annoying.decorators import ajax_request
-from django.views.generic import TemplateView, ListView,DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 
-from Insta.models import Post, Like, InstaUser
+from Insta.models import Post, Like, InstaUser, UserConnection
 from Insta.forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -14,9 +14,18 @@ class HelloWorld(TemplateView):
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
 class PostDetailView(DetailView):
-    model = Post
+    model = InstaUser
     template_name = 'post_detail.html'
+class UserDetailView(DetailView):
+    model = Post
+    template_name = 'user_detail.html'
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
@@ -33,7 +42,7 @@ class PostDeleteView(DeleteView):
 class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("login")   
     
 @ajax_request
 def addLike(request):
